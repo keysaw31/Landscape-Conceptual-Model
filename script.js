e03ac341c4957658feac17c9fd86d82da5035cb1
Joey Kiesau <kiesau31@gmail.com>
2023-10-29 07:59:00 -0500

    Clean up

--- script.js
+++ script.js
@@ -11,28 +11,5 @@ function toggleMenu(x) {
     }, 3000); // Adjust the delay (in milliseconds) as needed
 
 
-    // Handling submission
-    document.querySelector('.contact-form').addEventListener('submit', function(e) {
-        e.preventDefault(); // Prevents the form from actually submitting
-    
-        var formData = new FormData(this);
-    
-        fetch('process_form.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            if (data === 'success') {
-                alert('Message sent successfully!');
-                // You can add more actions for successful submission here
-            } else {
-                alert('Error sending message. Please try again later.');
-                // You can add more actions for error here
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    });
+   
     
